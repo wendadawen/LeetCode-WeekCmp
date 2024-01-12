d94bfe3f944dbdccca8f6c8edbfe67550fbdeb0e
wendadawen <wendadawen@qq.com>
2024-01-12 19:19:26 +0800

    init

---
+++ b.cpp
@@ -0,0 +1,4 @@
+#include <bits/stdc++.h>
+#define debug(x) cout<<"LINE:"<<__LINE__<<" "<<#x<<"="<<x<<endl;
+using namespace std;using ll=long long;using pii=pair<int,int>;
+const int INF=numeric_limits<int>::max();const int P=1e9+7;
