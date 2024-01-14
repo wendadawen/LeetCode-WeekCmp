61e1beb594f72343794784e7895857041814f148
wendadawen <wendadawen@qq.com>
2024-01-14 16:21:29 +0800

    329周赛第四题补题

--- 379-signal/d.cpp
+++ 379-signal/d.cpp
@@ -4,27 +4,50 @@ using namespace std;using ll=long long;using pii=pair<int,int>;
 const int INF=numeric_limits<int>::max();const int P=1e9+7;
 class Solution {
 public:
-    int maximumSetSize(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size(); int nn = n/2;
-        unordered_map<int, int> cnt1, cnt2, cnt;
-        for(auto& x: nums1) cnt1[x]++;
-        for(auto& x: nums2) cnt2[x]++;
-        int c1 = 0, c2 = 0;
-        vector<int> a;
-        for(auto&[u, v]: cnt1) {
-            if(cnt2.count(u)==0) {
-                if(c1 < nn)cnt[u]=1, c1++;
-            } else a.push_back(u);
-        }
-        for(auto&[u, v]: cnt2) {
-            if(cnt1.count(u)==0 && c2<nn) cnt[u]=1, c2++;
-        }
-        if(cnt.size() == n) return n;
-        for(auto& x: a) {
-            if(c1 < c2 && c1 < nn) cnt[x]=1, c1++;
-            else if(c2 < c1 && c2 < nn) cnt[x]=1, c2++;
-            else if(c1 == c2 && c1 < nn) cnt[x]=1, c1++;
-        }
-        return cnt.size();
+    int maxPartitionsAfterOperations(string s, int k) {
+        if (k >= 26)
+            return 1;
+        int n = s.size();
+        auto get = [&](int x) -> int {
+            int ret = 0;
+            while (x){
+                if (x & 1)
+                    ret++;
+                x >>= 1;
+            }
+            return ret;
+        };
+        unordered_map<long long, int> mp;
+        auto dfs = [&](auto dfs, int i, int mask, int changed) -> int {
+            if (i == n)
+                return 1;
+            int a = s[i] - 'a';
+            long long args_mask = (long long) i << 32 | mask << 1 | changed;
+            if(mp.count(args_mask)) return mp[args_mask];
+            int ret = 0;
+            if ((mask >> a) & 1)
+                ret = max(ret, dfs(dfs, i + 1, mask, changed));
+            else {
+                if (get(mask) == k)
+                    ret = max(ret, dfs(dfs, i + 1, (1 << a), changed) + 1);
+                else
+                    ret = max(ret, dfs(dfs, i + 1, mask | (1 << a), changed));
+            }
+            if (changed == 0) {
+                for (int j = 0; j < 26; j++) {
+                    a = j;
+                    if ((mask >> a) & 1)
+                        ret = max(ret, dfs(dfs, i + 1, mask, 1));
+                    else {
+                        if (get(mask) == k)
+                            ret = max(ret, dfs(dfs, i + 1, (1 << a), 1) + 1);
+                        else
+                            ret = max(ret, dfs(dfs, i + 1, mask | (1 << a), 1));
+                    }
+                }
+            }
+            return mp[args_mask] = ret;
+        };
+        return dfs(dfs, 0, 0, 0);
     }
 };
